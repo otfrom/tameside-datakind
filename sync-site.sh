942538369fd3a1caee948ff69d54561f82f672a0
Bruce Durling <bld@otfrom.com>
2013-07-28 11:00:32 +0100

    Changed site sync script.

--- sync-site.sh
+++ sync-site.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-s3cmd sync --exclude-from=./exclusions.txt ./ s3://${1} 
+s3cmd sync --exclude '.DS_Store' --exclude '.git/*' --exclude-from '.gitgnore' --exclude-from=exclusions.txt ./ s3://${1} --acl-public --delete-removed
